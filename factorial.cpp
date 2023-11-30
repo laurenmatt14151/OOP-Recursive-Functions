4dcbb59392a8e30a8e4435ef76f21788de80d5b4
Lauren Matthews <126510096+laurenmatt14151@users.noreply.github.com>
2023-11-30 13:39:39 -0500

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
 
 using namespace std;
-/*
-  0, 1, 1, 2, 3, 5, 8
-  Base Case: 0, 1
-  
-  General Case: x + y
-  F(n)
-  F(4) = 2
-  F(5) = F(3) + F(4)
-  F(n) = F(n-2) + F(n-1)
-*/
+
 int fact(int n)
 if(n== 0)
 {
